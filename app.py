4e8adaa61071ff12196da7e1e4ec24b73ae3ab5f
Rishabh Dixit <25567873+rdrishabh38@users.noreply.github.com>
2025-09-07 04:27:28 +0530

    updated LICENSE requirements

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+# Dota 2 Analytics Hub
+# A personal desktop dashboard to download, analyze, and visualize your Dota 2 metadata.
+# Copyright (C) 2025 Kocha rdrishabh38@gmail.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 import streamlit as st
 import pandas as pd
 import json
@@ -203,3 +221,10 @@ with tab2:
                 save_config(config)
                 st.success(f"Deleted profile: '{selected_profile_to_edit}'")
                 st.rerun()
+
+
+st.markdown("---")
+st.caption(
+    "This program is licensed under the GNU General Public License v3.0. "
+    "Copyright (C) 2025 Kocha."
+)
